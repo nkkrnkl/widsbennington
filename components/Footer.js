d78f6d8cf05ffbb37e8f253ed940caeddb4144c9
Niki Karanikola <70606645+nkkrnkl@users.noreply.github.com>
2023-01-18 23:55:01 +0200

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -5,8 +5,8 @@ export default function Footer() {
     <>
       
       <footer className={styles.footer}>
-        <a href="https://www.linkedin.com/company/wids-bennington/"><img src="https://www.edigitalagency.com.au/wp-content/uploads/Linkedin-logo-png-white.png" alt="LinkedIn icon" width="50" height="50"/></a>
-        <a href = "mailto: nikikaranikola@bennington.edu"><img src="https://i.pinimg.com/originals/50/06/dc/5006dc7319d99a139727482e4d75b1f2.jpg" alt="LinkedIn icon" width="50" height="50"/></a>   
+        <a href="https://www.linkedin.com/company/wids-bennington/"><img src="https://www.edigitalagency.com.au/wp-content/uploads/Linkedin-logo-png-white.png" alt="LinkedIn icon" width="40" height="40"/></a>
+        <a href = "mailto: nikikaranikola@bennington.edu"><img src="https://i.pinimg.com/originals/50/06/dc/5006dc7319d99a139727482e4d75b1f2.jpg" alt="LinkedIn icon" width="60" height="60"/></a>   
       
         @ 2023 Niki Karanikola. All Rights Reserved.
       </footer>
