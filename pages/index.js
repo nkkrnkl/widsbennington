c3e90cc53a17a49d85da85a1600add4c350815cd
Niki Karanikola <70606645+nkkrnkl@users.noreply.github.com>
2023-01-18 21:17:30 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,10 @@ export default function Home() {
 
       <main>
         <Header title="WiDS Bennington" />
+        <h2> Mission </h2>
         <p className="description">
-          Mission
+          <a href="https://www.widsconference.org/">Women in Data Science (WiDS)</a>
+          elevates women in the field by providing inspiration, education, community, and support.
         </p>
   
       </main>
