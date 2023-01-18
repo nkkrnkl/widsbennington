8fee125f4cc07554890a60b5db26407ee5cf5eb7
Niki Karanikola <70606645+nkkrnkl@users.noreply.github.com>
2023-01-18 21:07:46 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,17 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>WiDS Bennington</title>
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="WiDS Bennington" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <h2>Mission</h2><br>
+          <a href="https://www.widsconference.org">Women in Data Science (WiDS)</a> elevates women in the field by providing inspiration, education, community, and support. <br>  
+          <h2> WiDS Bennington encourages people to get out of their comfort zone and try something NEW.</h2>
         </p>
+  
       </main>
 
       <Footer />
