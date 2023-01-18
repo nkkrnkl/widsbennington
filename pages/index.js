30c67378d8c114dc9f783f74a23347bcc2efaf6e
Niki Karanikola <70606645+nkkrnkl@users.noreply.github.com>
2023-01-18 21:55:32 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,15 +18,20 @@ export default function Home() {
         </p>
         <h3>WiDS Bennington encourages people to get out of their comfort zone and try something NEW.</h3>
         <h3>Coming Events</h3> 
-        <p className="description">
-          1. Datathon Workshop: Saturday 25th of February Learn how to use Data Science to Improving Extreme Weather Forecasts with 
-          <a href="https://www.kaggle.com/competitions/widsdatathon2023">WiDS Datathon 2023</a> 
-          challenge. All levels are welcome to participate. 
-          <a href="https://docs.google.com/forms/d/e/1FAIpQLSfH7sbClZiz0Q_XnzqDbqk-ulFwKGxWjJAYPspHN_Xfyw0wqg/viewform?usp=sf_link">Register here now.</a> 
+        
+        <p className="list">
+          <dl>
+            <dt>1. Datathon Workshop</dt>
+            <dd>Saturday 25th of February Learn how to use Data Science to Improving Extreme Weather Forecasts with 
+              <a href="https://www.kaggle.com/competitions/widsdatathon2023">WiDS Datathon 2023 </a> 
+              challenge. All levels are welcome to participate. 
+              <a href="https://docs.google.com/forms/d/e/1FAIpQLSfH7sbClZiz0Q_XnzqDbqk-ulFwKGxWjJAYPspHN_Xfyw0wqg/viewform?usp=sf_link"> Register here now.</a></dd>
+            <dt>2. Data Science Conference</dt>
+            <dd>more info coming soon...</dd>
+          </dl>
+           
         </p>
-        <p className="description">
-          2. Data Science Conference: more info coming soon...
-        </p>
+        
   
       </main>
 
