8afa69dbfa79ebeed108fe332cffb9472012e4e3
Niki Karanikola <70606645+nkkrnkl@users.noreply.github.com>
2023-01-19 12:40:28 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
-export default function Header({ title }) {
+export default function Header({ title, test }) {
   return <
-      <h1> test </h1>
+      <h1> {test} </h1>
 //       <img src="public/wids-black-stacked.png" alt="logo" width="50" height="50">
       <h1 className="title">{title}</h1>
     > 
