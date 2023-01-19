3972845d92a1c57458c583269c35de2d14f0447d
Niki Karanikola <70606645+nkkrnkl@users.noreply.github.com>
2023-01-19 14:00:37 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
 export default function Header() {
-  return <img src="/wids-black-1line.png" alt="logo" width="150" height="60"/>
+  return <img src="/wids-black-1line.png" alt="logo" width="250" height="90"/>
 //   return <h1 className="title"><img src="/wids-black-1line.png" alt="logo" width="100" height="50"/> {title}</h1>
 }
