1cb8f1b9b5ace2ff2c941ceefceff3c2234fff6d
Niki Karanikola <70606645+nkkrnkl@users.noreply.github.com>
2023-02-14 18:53:30 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -38,17 +38,10 @@ export default function Home() {
               <a href="https://docs.google.com/forms/d/e/1FAIpQLSfH7sbClZiz0Q_XnzqDbqk-ulFwKGxWjJAYPspHN_Xfyw0wqg/viewform?usp=sf_link"> Register here now.</a></dd>
             <dt className="title">2. Data Science Conference</dt>
             <dd>On Wednesday 8th of March join us at 11.00 am to 4.35 pm in Bennington College, CAPA Symposium. You will listen to Stanford Universty's WiDS Conference, which will feature keynotes, technical vision talks,
-fireside chats, and networking. <a href="https://www.eventbrite.com/e/women-in-data-science-wids-conference-bennington-2023-tickets-546647867487"> Register here now.</a>
+fireside chats, and networking. Registration coming soon.
             </dd>
-            </dd>
-          </dl>  
-        </p>
-        
-        <p> 
-            <a href="https://www.linkedin.com/company/wids-bennington/"><img src="https://w7.pngwing.com/pngs/940/589/png-transparent-linkedin-free-text-telephone-call-trademark.png"></a>
-            <a href="https://www.instagram.com/widsbenn23/"><img src="https://www.pngfind.com/pngs/m/2-23339_black-and-white-instagram-logo-instagram-logo-2018.png"></a>
-            <a href="https://www.facebook.com/widsbennington23"><img src="https://www.pngfind.com/pngs/m/57-579617_facebook-logo-circle-black-transparent-social-media-icons.png"></a>
-            <a href="mailto:nikikaranikola@bennington.edu"><img src="https://w7.pngwing.com/pngs/111/717/png-transparent-computer-icons-gmail-google-logo-gmail-angle-text-triangle.png"></a>
+          </dl>
+           
         </p>
         
   
