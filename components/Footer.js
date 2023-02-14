1e9e10867b0d7079efff65407b372f12bea7b916
Niki Karanikola <70606645+nkkrnkl@users.noreply.github.com>
2023-02-14 18:47:30 -0500

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -2,11 +2,8 @@ import styles from './Footer.module.css'
 
 export default function Footer() {
   return (
-    <>
-      
       <footer className={styles.footer}>
         @ 2023 Niki Karanikola. All Rights Reserved.
       </footer>
-    </>
   )
 }
