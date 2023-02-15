17c16c1480ac97fb4d49c204ee010919d8f8aac9
Niki Karanikola <70606645+nkkrnkl@users.noreply.github.com>
2023-02-14 19:54:23 -0500

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,19 +4,19 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-          <div style="float:left;margin-right:5px;">
+          <div className={styles.logo}>
             <a href="https://www.linkedin.com/company/wids-bennington/"><img src="/linkedin.png" alt="LinkedIn icon" width="30" height="30"/></a>
           </div>
-          <div style="float:left;margin-right:5px;">
+          <div className={styles.logo}>
             <a href="https://www.instagram.com/widsbenn23/"><img src="/instagram.png" alt="Instagram icon" width="25" height="25"/></a>
           </div>
-          <div style="float:left;margin-right:5px;">
+          <div className={styles.logo}>
             <a href="https://www.facebook.com/widsbennington23/"><img src="/facebook.png" alt="Facebook icon" width="25" height="25"/></a>
           </div>
-          <div style="float:left;margin-right:5px;">
+          <div className={styles.logo}>
             <a href = "mailto: nikikaranikola@bennington.edu"><img src="/mail.png" alt="Gmail icon" width="30" height="30"/></a>        
           </div>
-          <div style="float:left;margin-right:5px;">
+          <div className={styles.logo}>
             <p>@ 2023 Niki Karanikola. All Rights Reserved.</p>
           </div>       
       </footer>
